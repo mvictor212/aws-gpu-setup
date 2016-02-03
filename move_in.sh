1916498081eb9136a10f8d34f01168f813dd2047
Mason Victors <mason.victors@gmail.com>
2016-02-03 07:14:04 -0700

    Remove symbolic linking of bash files

--- move_in.sh
+++ move_in.sh
@@ -2,18 +2,18 @@ if [ -f ~/.bashrc ]
 then
   mv ~/.bashrc ~/.bashrc.bak
 fi
-ln -s .bashrc ~/.bashrc
+cp .bashrc ~/.bashrc
 
 if [ -f ~/.bash_profile ]
 then
   mv ~/.bash_profile ~/.bash_profile.bak
 fi
-ln -s .bash_profile ~/.bash_profile
+cp .bash_profile ~/.bash_profile
 
 if [ -d ~/.bash.d ]
 then
   mv ~/.bash.d ~/.bash.d.bak
 fi
-ln -s .bash.d ~/.bash.d
+cp -r .bash.d ~/.bash.d
 
 source ~/.bashrc
