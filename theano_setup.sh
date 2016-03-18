13cfbf59da23682172eb6552456330b946354069
Mason Victors <mason.victors@recursionpharma.com>
2016-03-18 15:27:04 +0000

    Remove installs of matplotlib and bokeh

--- theano_setup.sh
+++ theano_setup.sh
@@ -8,4 +8,4 @@ make
 
 echo -e "\n[global]\nfloatX=float32\ndevice=gpu\n[mode]=FAST_RUN\n\n[nvcc]\nfastmath=True\n\n[cuda]\nroot=/usr/local/cuda" > ~/.theanorc
 
-sudo pip install ipython jupyter pandas keras matplotlib bokeh
+sudo pip install ipython jupyter pandas keras
