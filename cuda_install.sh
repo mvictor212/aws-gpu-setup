af4656be4b0cfe5229e98cc9005799bc40a2afef
Mason Victors <mason.victors@recursionpharma.com>
2016-03-18 15:05:14 +0000

    Fix errors with certain installs

--- cuda_install.sh
+++ cuda_install.sh
@@ -1,7 +1,9 @@
 sudo apt-get update
 sudo apt-get -y dist-upgrade
-sudo apt-get install -y gcc g++ gfortran build-essential git wget linux-image-generic libopenblas-dev python-dev python-pip python-nose python-numpy python-scipy
+sudo apt-get install -y gcc g++ gfortran build-essential git wget linux-image-generic libopenblas-dev python-dev python-pip python-nose libblas-dev liblapack-dev libatlas-base-dev libfreetype6-dev
 
+sudo pip install numpy
+sudo pip install scipy
 sudo pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
 
 sudo wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.0-28_amd64.deb
--- theano_setup.sh
+++ theano_setup.sh
@@ -6,6 +6,6 @@ cd NVIDIA\_CUDA-7.5\_Samples/1\_Utilities/deviceQuery
 make  
 ./deviceQuery
 
-echo -e "\n[global]\nfloatX=float32\ndevice=gpu\n[mode]=FAST_RUN\n\n[nvcc]\nfastmath=True\n\n[cuda]\nroot=/usr/local/cuda" >> ~/.theanorc
+echo -e "\n[global]\nfloatX=float32\ndevice=gpu\n[mode]=FAST_RUN\n\n[nvcc]\nfastmath=True\n\n[cuda]\nroot=/usr/local/cuda" > ~/.theanorc
 
-sudo pip install ipython jupyter pandas keras
+sudo pip install ipython jupyter pandas keras matplotlib bokeh
